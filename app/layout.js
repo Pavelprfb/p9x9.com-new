f7dfd0321956ff03bdb6b883084052c0d493d4a9
Pavelprfb <pavelprfb@gmail.com>
2026-08-20 22:20:24 +0600

    Add Google tag gtag.js (G-T9KY79TYK4) to head

--- app/layout.js
+++ app/layout.js
@@ -54,6 +54,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T9KY79TYK4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-T9KY79TYK4');`
+          }}
+        />
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TNK4FPN5');` }} />
         {/* End Google Tag Manager */}
